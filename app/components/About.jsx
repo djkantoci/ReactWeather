d70331455079fb3f65119b63d2acd27586661aa7
George <djkantoci@aut-aut.hr>
2017-02-22 22:43:24 +0100

    Adding content to About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,23 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to about page, yeeeea!</p>
+      <h1 className="text-center">About</h1>
+      <p>This is Weather React application, yeeeea!</p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react" target="_blank">
+            React JavaScript framework
+          </a>
+        </li>
+        <li>
+          <a href="http://openweathermap.com" target="_blank">
+            Source of Weather
+          </a>
+        </li>
+      </ul>
     </div>
   )
 };
